4ce387b090697005aea8cd7cb62fd46e87e99467
Joey <jpg@neilpie.co.uk>
2021-04-09 15:24:37 +0100

    added in functionality for custom placeholders

--- helper-functions.php
+++ helper-functions.php
@@ -9,13 +9,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Send a single email (no trigger required)
  *
- * @param WP_Post $email_post Email post object
- * @param WP_User $user       User object
+ * @param WP_Post $email_post   Email post object
+ * @param WP_User $user         User object
+ * @param array   $placeholders Any placeholders that need to be replaced
  */
-function pce_send_email( $email_post, $user ) {
+function pce_send_email( $email_post, $user, $placeholders = array() ) {
   $to      = $user->user_email;
   $subject = get_the_title( $email_post );
-  $body    = pce_process_placeholders( get_the_content( null, false, $email_post ), $user );
+  $body    = pce_process_placeholders( get_the_content( null, false, $email_post ), $user, $placeholders );
   $headers = array( 'Content-Type: text/html; charset=UTF-8' );
   return wp_mail( $to, $subject, $body, $headers );
 }
@@ -23,10 +24,11 @@ function pce_send_email( $email_post, $user ) {
 /**
  * Send emails for certain trigger
  *
- * @param int/string  $trigger Trigger (tag) ID/slug
- * @param int/WP_User $user    User ID or User Object
+ * @param int/string  $trigger      Trigger (tag) ID/slug
+ * @param int/WP_User $user         User ID or User Object
+ * @param array       $placeholders Any placeholders that need to be replaced
  */
-function pce_send_emails( $trigger = '', $user = 0 ) {
+function pce_send_emails( $trigger = '', $user = 0, $placeholders = array() ) {
   if ( ! term_exists( $trigger, 'trigger' ) ) {
     return new WP_Error( 'pie-custom-emails', 'No trigger found with ID/slug: ' . $trigger );
   }
@@ -43,7 +45,7 @@ function pce_send_emails( $trigger = '', $user = 0 ) {
 	}
   // loop through emails (post objects) and send
   foreach ( $emails as $email_post ) {
-    pce_send_email( $email_post, $user );
+    pce_send_email( $email_post, $user, $placeholders );
   }
 }
 
@@ -71,21 +73,18 @@ function pce_get_emails( $trigger = '' ) {
 /**
  * Parse content to replace placeholders with User info
  *
- * @param string  $content Email content
- * @param WP_User $user    User object
+ * @param string  $content      Email content
+ * @param WP_User $user         User object
+ * @param array   $placeholders Any placeholders that need to be replaced
  */
-function pce_process_placeholders( $content, $user ) {
-  $first_name     = get_user_meta( $user->ID, 'first_name', true ) ? get_user_meta( $user->ID, 'first_name', true ) : '{{user.first_name}}';
-  $last_name      = get_user_meta( $user->ID, 'last_name', true ) ? get_user_meta( $user->ID, 'last_name', true ) : '{{user.last_name}}';
-  return str_replace( array(
-    '{{user.first_name}}',
-    '{{user.last_name}}',
-    '{{user.email}}',
-    ), array(
-      $first_name,
-      $last_name,
-      $user->user_email
-    ),
-    $content
-  );
+function pce_process_placeholders( $content, $user, $placeholders = array() ) {
+  $first_name = get_user_meta( $user->ID, 'first_name', true ) ? get_user_meta( $user->ID, 'first_name', true ) : '{{user.first_name}}';
+  $last_name  = get_user_meta( $user->ID, 'last_name', true ) ? get_user_meta( $user->ID, 'last_name', true ) : '{{user.last_name}}';
+  $defaults   = array(
+		'{{user.first_name}}' => $first_name,
+    '{{user.last_name}}'  => $last_name,
+    '{{user.email}}'      => $user->user_email,
+	);
+	$to_replace = wp_parse_args( $placeholders, $defaults );
+  return str_replace( array_keys( $to_replace ), array_values( $to_replace ), $content );
 }
