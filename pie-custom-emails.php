8e4f6e1eb791ee794c1b3ce5113c44b0b807f3e7
Bradley Hastings <bradleybeasley2000@gmail.com>
2023-08-18 11:10:46 +0100

    Reformat code

--- pie-custom-emails.php
+++ pie-custom-emails.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Custom Emails
  * Plugin URI: #
@@ -8,48 +9,53 @@
  * Author URI: https://pie.co.de
  * Text Domain: pie-custom-emails
  */
- if ( ! defined( 'ABSPATH' ) ) {
- 	exit; // Exit if accessed directly
- }
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
 
- if ( ! class_exists( 'PIE_Custom_Emails' ) ) {
-   class PIE_Custom_Emails {
+if (!class_exists('PIE_Custom_Emails')) {
+    class PIE_Custom_Emails
+    {
+        /**
+         * The single instance of the class
+         *
+         * @var PIE_Custom_Emails
+         */
+        protected static $_instance = null;
 
-     /**
-    	* The single instance of the class
-    	*
-    	* @var PIE_Custom_Emails
-    	*/
-    	protected static $_instance = null;
+        /**
+         * PIE_Custom_Emails __construct
+         */
+        public function __construct()
+        {
+            $this->load_plugin();
+        }
 
-     /**
-      * PIE_Custom_Emails __construct
-      */
-     public function __construct() {
-       $this->load_plugin();
-     }
+        /**
+         * Load plugin
+         */
+        public function load_plugin()
+        {
+            require_once 'helper-functions.php';
+            require_once 'classes/pie-custom-emails-register.php';
+            if (class_exists('PIE_Custom_Emails_Register')) {
+                $register = new PIE_Custom_Emails_Register();
+                $register->init();
+            }
+        }
 
-     /**
-      * Load plugin
-      */
-     public function load_plugin() {
-       require_once 'helper-functions.php';
-       require_once 'classes/pie-custom-emails-register.php';
-       $register = new PIE_Custom_Emails_Register;
-       $register->init();
-     }
-
-     /**
-  	  * Plugin Instance
-      *
-  	  * @return PIE_Custom_Emails Main instance
-  	  */
-    	public static function instance() {
-    		if ( is_null( self::$_instance ) ) {
-    			self::$_instance = new self();
-    		}
-    		return self::$_instance;
-    	}
-   }
-   PIE_Custom_Emails::instance();
- }
+        /**
+         * Plugin Instance
+         *
+         * @return PIE_Custom_Emails Main instance
+         */
+        public static function instance()
+        {
+            if (is_null(self::$_instance)) {
+                self::$_instance = new self();
+            }
+            return self::$_instance;
+        }
+    }
+    PIE_Custom_Emails::instance();
+}
